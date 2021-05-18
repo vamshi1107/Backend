8b996ba9b38c0bb207ef02b454d20d124d432707
= <vamshi.a4u@gmail.com>
2021-05-18 16:02:05 +0530

    port

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express")
 
 const app=express()
+const port=process.env.port || 1107
 
 app.get("/",(req,res)=>{
     res.send("This is  server of vamshi addanki ")
 })
 
-app.listen(1107,()=>{
+app.listen(port,()=>{
     console.log("Listening at 1107")
 })
