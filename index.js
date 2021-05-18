96a3e5bf169edd85a2cf9a6882df754544991a13
= <vamshi.a4u@gmail.com>
2021-05-18 17:48:47 +0530

    home page

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express = require("express")
 const emailRouter = require("./mail")
+const fs =require("fs")
 const cors=require('cors')
 const app=express()
 const port=process.env.PORT || 5000
+
+const indexPage=fs.readFileSync("index.html").toString()
 
 app.use("/mail",emailRouter)
 app.use(cors({}))
 
 
+
 app.get("/",(req,res)=>{
-    res.send("This is  server of vamshi addanki ")
+  res.send(indexPage)
 })
 
-app.listen(port,(data,err)=>{
+app.listen(port,()=>{
     console.log("Listening at "+port)
 })
