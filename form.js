d7e8c65509048c0c1d7c810373ac240ea8e25c69
Vamshi Addanki <vamshi.a4u@gmail.com>
2021-09-13 17:31:49 +0530

    added new

--- form.js
+++ form.js
@@ -1,3 +1,4 @@
+const e = require('express')
 const  express =require('express')
 const mongoose =require('mongoose')
 const { all } = require('./mail')
@@ -24,17 +25,76 @@ formrouter.post("/add",async (req,res)=>{
                        }
                       });
                      result=await collection.updateOne({"formid":data.formid},{$set:{...ex}})
-                     res.send({"status":2,"modifiedCount": 1})
+                     res.send({"status":2,"modifiedCount": result.modifiedCount})
                  }
                 else{
                      result=await collection.insertOne(data)
-                     res.send({"status":1,"insertedCount": 1})
+                     res.send({"status":1,"insertedCount": result.insertedCount})
                   }
              }
          })
 
 })
 
+
+formrouter.post("/addresp",async (req,res)=>{
+         const con=await mongoose.connection
+         const collection =con.db.collection("myforms_response")
+         const data=req.body
+         var result
+         result=await collection.insertOne(data)
+         res.send({"status":1,"insertedCount": result.insertedCount})
+})
+
+formrouter.post("/showresp",async (req,res)=>{
+         const con=await mongoose.connection
+         const collection =con.db.collection("myforms_response")
+         const data=req.body
+        await collection.find({"formid":data.formid},async (err,dt)=>{
+            if(err){
+              res.send("error")
+            }
+            else{
+                var con=await dt.toArray()
+                res.send(con)
+            }
+         })
+})
+
+formrouter.post("/getresp",async (req,res)=>{
+         const con=await mongoose.connection
+         const collection =con.db.collection("myforms_response")
+         const data=req.body  
+         var form
+         var resp;
+         var q={"respid":data.respid}
+         await con.db.collection("myforms_forms").findOne({"formid":data.formid },async (err,dt)=>{
+                 if(!err){ 
+                   form=dt
+                   await collection.findOne(q,async (err2,dt2)=>{
+                     if(!err){
+                       resp=dt2
+                       form["feilds"].map(ele=>{
+                        ele.type==1?ele.value=getValue(resp,ele.fid):
+                                    ele.value=getValue(resp,ele.fid),
+                                    ele.oid=getOption(resp,ele.fid)
+                                  }
+                        )
+                       res.send(form)
+                      }
+                  }
+                  )
+                 }
+          })
+})
+
+function getValue(resp,id){
+     return resp["responses"].filter(ele=>ele.fid==id)[0].response
+}
+
+function getOption(resp,id){
+     return resp["responses"].filter(ele=>ele.fid==id)[0].oid
+}
 
 formrouter.post("/get",async (req,res)=>{
          const con=await mongoose.connection
